5fffa00dcfd33f0c1add46ffdc0fe3efae0c16ae
Umut <umutkarabulut.personal@gmail.com>
2023-11-11 17:02:31 +0100

    pause, resume and stop options added.

--- timer.py
+++ timer.py
@@ -1,13 +1,31 @@
 import time
+import keyboard
 
 def timer(seconds):
+    paused = False
     while seconds > 0:
-        print("\r", end="")
-        minutes_left = seconds // 60
-        seconds_left = seconds % 60
-        print(f"{minutes_left:02d}:{seconds_left:02d}", end="")
-        time.sleep(1)
-        seconds -= 1
+        if not paused:
+            print("\r", end="")
+            minutes_left = seconds // 60
+            seconds_left = seconds % 60
+            print(f"{minutes_left:02d}:{seconds_left:02d}", end="")
+            time.sleep(1)
+            seconds -= 1
+        if keyboard.is_pressed('p'):
+            paused = True
+            print("\nTimer is paused, press 'r' to resume.")
+            while paused:
+                if keyboard.is_pressed('r'):
+                    paused = False
+                    print("\rResuming Timer...")
+        if keyboard.is_pressed('s'):
+            print("\nTimer is stopped.")
+            break
+    else:
+        print("\nTime us up!")
+
+
+print("Press 'p' to pause, 'r' to resume and 's' to stop the timer." )
 
 print("Here you must declare the time you want for the Timer!")
 
@@ -21,5 +39,5 @@ if seconds < 0 or seconds > 59:
 
 total_seconds = minutes * 60 + seconds
 timer(total_seconds)
-print("\nTime is up!")
+
 
