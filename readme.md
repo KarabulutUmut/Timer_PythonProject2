7f55434b40514ad566236e1a0ebad3f9246ad7e6
Umut <umutkarabulut.personal@gmail.com>
2023-11-21 00:38:00 +0100

    threading

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Timer
     
-    Basic timer. Pause, stop and resume options added in v2.
-    
+    Basic timer. 
+    Pause, stop and resume options added in v2.
+    When both operations(timer, input) were working in one function, it wasn't catching the inputs perfectly, so Threading added in v3. 
     A GUI will be added!
--- timer.py
+++ timer.py
@@ -1,29 +1,37 @@
 import time
 import keyboard
+import threading
 
+paused = False
+stopped = False
 def timer(seconds):
-    paused = False
-    while seconds > 0:
+    global paused, stopped
+    while seconds > 0 and not stopped:
         if not paused:
-            print("\r", end="")
+            #print("\r", end="")
             minutes_left = seconds // 60
             seconds_left = seconds % 60
-            print(f"{minutes_left:02d}:{seconds_left:02d}", end="")
+            print(f"\r{minutes_left:02d}:{seconds_left:02d}", end="")
             time.sleep(1)
             seconds -= 1
-        if keyboard.is_pressed('p'):
-            paused = True
-            print("\nTimer is paused, press 'r' to resume.")
-            while paused:
-                if keyboard.is_pressed('r'):
-                    paused = False
-                    print("\rResuming Timer...")
-        if keyboard.is_pressed('s'):
-            print("\nTimer is stopped.")
-            break
+    if stopped:
+        print("\nTimer is stopped.")
     else:
         print("\nTime us up!")
 
+def keyboard_input():
+    global paused, stopped
+    while True:
+        if keyboard.is_pressed('p') and not paused:
+            paused = True
+            print("\nTimer is paused, press 'r' to resume.")
+        elif keyboard.is_pressed('r') and paused:
+            paused = False
+            print("\rResuming Timer...")
+        elif keyboard.is_pressed('s'):
+            stopped = True
+            break
+        time.sleep(0.1) # Preventing high CPU usage
 
 print("Press 'p' to pause, 'r' to resume and 's' to stop the timer." )
 
@@ -38,6 +46,12 @@ if seconds < 0 or seconds > 59:
     raise ValueError("Seconds must be between 0 and 59.")
 
 total_seconds = minutes * 60 + seconds
-timer(total_seconds)
 
+timer_thread = threading.Thread(target=timer, args=(total_seconds,))
+keyboard_thread = threading.Thread(target=keyboard_input)
 
+timer_thread.start()
+keyboard_thread.start()
+
+timer_thread.join()
+keyboard_thread.join()
